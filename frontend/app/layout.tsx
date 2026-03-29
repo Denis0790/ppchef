f6b2c2edeaa43bdb07d020eecb338e75c8dc23bb
Papaden9 Taka9ze <papadentakaze@gmail.com>
2026-03-29 11:01:16 +0300

    servisWorker kick

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -69,7 +69,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <AuthProvider>
           <OfflineBanner />
-          <ServiceWorkerRegister />
           <IOSInstallBanner />
           {children}
           <BottomNavWrapper />
