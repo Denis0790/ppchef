5a2b35e2da45785079984229ee60d5e42b53c280
Papaden9 Taka9ze <papadentakaze@gmail.com>
2026-04-11 14:42:30 +0300

    add yandexMetrica

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -9,6 +9,7 @@ import IOSInstallBanner from "@/components/IOSInstallBanner";
 import OfflineBanner from "@/components/OfflineBanner";
 import RefCodeSaver from "@/components/RefCodeSaver";
 import { Suspense } from "react";
+import Script from "next/script";
 
 const cormorant = Cormorant_Garamond({
   subsets: ["cyrillic"],
@@ -98,6 +99,35 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <BottomNavWrapper />
           <CookieBanner />
         </AuthProvider>
+
+        {/* ── ЯНДЕКС МЕТРИКА ── */}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {
+              if (document.scripts[j].src === r) { k=document.scripts[j]; break; }
+            }
+            k=k||e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+            ym(108497387, "init", {
+              clickmap: true,
+              trackLinks: true,
+              accurateTrackBounce: true,
+              webvisor: true
+            });
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/108497387"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
+
       </body>
     </html>
   );
